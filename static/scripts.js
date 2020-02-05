7a891cc2031804a65327796ced0177f4c1d2d111
Ryan <rumeleazar@gmail.com>
2020-02-05 13:33:02 +0800

    Made profile section responsive and added a hover effect

---
+++ static/scripts.js
@@ -0,0 +1,33 @@
+
+// function recipeCardEnlarger() {
+// 	document.querySelector('.column h3').style.fontSize ='30px';
+// 	document.querySelector('.column').style.overflow = 'visible';
+// 	document.querySelector('.column').style.height = '500px';
+// 	document.querySelector('.column').style.width = '300px';
+	
+
+
+
+// }
+
+// function recipeCardNormalize(){
+// 	document.querySelector('.column h3').style.fontSize ='15px';
+// 	document.querySelector('.column').style.overflow = 'hidden';
+// 	document.querySelector('.column').style.height = '400px';
+// 	document.querySelector('.column').style.width = '250px';
+	
+
+
+	
+
+
+
+// }
+
+// // Making the recipe cards bigger on hover
+// document.querySelector('.column').addEventListener('mouseover', recipeCardEnlarger);
+
+// // Making the recipe cards return back to normal after hover
+
+// document.querySelector('.column').addEventListener('mouseover', recipeCardEnlarger);
+// document.querySelector('.column').addEventListener('mouseout', recipeCardNormalize);
