b928a227d8e3e6a21125bcfacd6cf6e23e6af6cd
Ryan <rumeleazar@gmail.com>
2020-03-13 00:14:06 +0800

    Full make over of website (Added many javascript features)

--- static/scripts.js
+++ static/scripts.js
@@ -16,50 +16,6 @@ for (let i = 0; i < allcolumns.length; i++) {
 
 
 
-//GETTING THE RANDOM QUOTES FROM THE WEB API
-//APPLY AJAX TO CHANGE THE QUOTE EVERY 6 SECONDS
-
-randomQuoteGenerator();
-var random = setInterval(randomQuoteGenerator, 10000);
-
-function randomQuoteGenerator () {
-
-var randomQuoteRequest = new XMLHttpRequest();
-randomQuoteRequest.open('GET', 'http://quotes.stormconsultancy.co.uk/random.json', true);
-randomQuoteRequest.onload = function () {
-	var quotes = JSON.parse(randomQuoteRequest.responseText);
-
-
-	var quote = document.querySelector('.quote');
-	var author = document.querySelector('.author');
-
-	quote.innerHTML = quotes.quote;
-	author.innerHTML = '-' + quotes.author;
-
-	//jQuery for Fade in and Fade out of random quotes
-	setTimeout(function() {
-
-		$(".quote").ready(function() {
-			setTimeout(function() {
-				$(".quote").fadeOut(3050);
-				$(".author").fadeOut(3050);
-			})
-
-			setTimeout(function() {
-				$(".quote").fadeIn(3000);
-				$(".author").fadeIn(3000);
-			},3050)
-
-		})
-		
-	}, 7000);
-
-	
-};
-
-randomQuoteRequest.send();
-
-};
 
 
 
@@ -84,6 +40,249 @@ window.onclick = function(event) {
 		$("nav ul li ul").slideUp("slow");
 	}
 }
+
+
+
+
+console.log(window.location.pathname);
+
+
+
+if (window.location.pathname == '/' || window.location.pathname =='/ryanulysses' ) {
+
+
+}
+
+
+
+
+// NAVIGATION BAR ANIMATION
+
+//NOTE: CHANGE THIS OPTION IF YOU KNOW HOW TO PASS JINJA DATA TO JAVASCRIPT
+
+window.addEventListener('scroll', function(){
+	var navBar = document.querySelector('.loginnav');
+	
+	if (window.location.pathname == '/' || window.location.pathname =='/ryanulysses' ) {
+
+		if (window.scrollY < 631) {
+
+		navBar.classList.remove("loginnav-fixed");
+		$('nav ul a').css('color','white');
+		$('nav a h2').css('color','white');
+		$('.searchbar input[type=text]').css('opacity','0.2');
+		$('.userprofilename').css('color','white');
+		$('.loginnav').css('position', 'fixed')
+		$('.loginnav').css('border-bottom', 'none');
+
+
+	} else {
+
+		navBar.classList.add("loginnav-fixed");
+		$('.userprofilename').css('color','#ead1ae');
+		$('nav ul a').css('color','#ead1ae');
+		$('nav a h2').css('color','#ead1ae');
+		$('.searchbar input[type=text]').css('opacity','0.8');
+		$('.loginnav').css('border-bottom', '1px solid #ead1ae');
+
+
+	}
+
+
+	
+	}	else {
+
+		if (window.scrollY == 0) {
+
+		navBar.classList.remove("loginnav-fixed");
+		$('nav ul a').css('color','white');
+		$('nav a h2').css('color','white');
+		$('.searchbar input[type=text]').css('opacity','0.2');
+		$('.userprofilename').css('color','white');
+		$('.loginnav').css('position', 'fixed')
+		$('.loginnav').css('border-bottom', 'none');
+
+
+	} else {
+
+		navBar.classList.add("loginnav-fixed");
+		$('.userprofilename').css('color','#ead1ae');
+		$('nav ul a').css('color','#ead1ae');
+		$('nav a h2').css('color','#ead1ae');
+		$('.searchbar input[type=text]').css('opacity','0.8');
+		$('.loginnav').css('border-bottom', '1px solid #ead1ae');
+
+
+	}
+	}
+
+
+
+
+	
+
+
+
+})
+
+
+
+
+// ANIMATION ON ARTICLE PAGE ON LOAD
+
+
+$( document ).ready(function() {
+    $('.dishnamesummarygroup').css('opacity', '1');
+    $('.dishnamesummarygroup').css('transform', 'translateY(0px)');
+});
+
+
+
+
+
+
+if ((window.location.href).includes('article')) {
+
+	// EASE IN AND OUT ANIMATION IN THE RECIPE SHOWCASE BOX	(ARTICLE.HTML)
+
+	function scrollAnimation2 () {
+
+	var animateIngredientsBox = document.querySelector('.ingredientsshowcase');
+	var animateDirectionsBox = document.querySelector('.directionsshowcase');
+	var animateCommentsBox = document.querySelector('.commentsArea');
+	var animateLinksBox = document.querySelector('.links');
+
+
+
+	var articleIngredientsPosition = animateIngredientsBox.getBoundingClientRect().top;
+	var articleDirectionsPosition = animateDirectionsBox.getBoundingClientRect().top;
+	var articleCommentsPosition = animateCommentsBox.getBoundingClientRect().top;
+	var articleLinksPosition = animateLinksBox.getBoundingClientRect().top;
+
+
+	if (articleIngredientsPosition <  window.innerHeight) {
+		animateIngredientsBox.classList.add("recipeshowcase-appear");
+	}
+
+	if (articleDirectionsPosition <  window.innerHeight) {
+		animateDirectionsBox.classList.add("recipeshowcase-appear");
+	}
+
+	if (articleCommentsPosition <  window.innerHeight) {
+		animateCommentsBox.classList.add("recipeshowcase-appear");
+	}
+
+	if (articleLinksPosition <  window.innerHeight) {
+		animateLinksBox.classList.add("recipeshowcase-appear");
+	}
+
+
+}
+
+window.addEventListener('scroll', scrollAnimation2);
+
+
+} else {
+
+	// EASE IN AND OUT ANIMATION IN THE ARTICLE BOX	(INDEX.HTML)
+
+		function scrollAnimation() {
+
+		var animateBannerBox = document.querySelector('.banner');
+		var animateIntroDescriptionBox = document.querySelector('.introdescription');
+		var animateIntroImageBox = document.querySelector('.introimage');
+		var animateRowBox = document.querySelector('.row');
+		var animateCollectionBox = document.querySelector('.recipecollectionheader');
+
+
+		var articleBannerPosition = animateBannerBox.getBoundingClientRect().top;
+		var articleIntroDescriptionPosition = animateIntroDescriptionBox.getBoundingClientRect().top;
+		var articleIntroImagePosition = animateIntroImageBox.getBoundingClientRect().top;
+		var articleRowPosition = animateRowBox.getBoundingClientRect().top;
+		var articleCollectionPosition = animateCollectionBox.getBoundingClientRect().top;
+
+
+
+		var screenHeight = window.innerHeight;
+		
+
+		if (articleBannerPosition < screenHeight) {
+			animateBannerBox.classList.add("articlebox-appear");
+		}
+
+		if (articleIntroDescriptionPosition < screenHeight) {
+			 animateIntroDescriptionBox.classList.add("articlebox-appear");
+		}
+
+		if (articleIntroImagePosition < screenHeight) {
+			 animateIntroImageBox.classList.add("articlebox-appear");
+		}
+
+		if (articleRowPosition < screenHeight) {
+			 animateRowBox.classList.add("articlebox-appear");
+		}
+
+		if (articleCollectionPosition < screenHeight) {
+			 animateCollectionBox.classList.add("articlebox-appear");
+		}
+
+
+		}
+
+
+		window.addEventListener('scroll', scrollAnimation);
+
+
+
+		//GETTING THE RANDOM QUOTES FROM THE WEB API
+		//APPLY AJAX TO CHANGE THE QUOTE EVERY 6 SECONDS
+
+		randomQuoteGenerator();
+		var random = setInterval(randomQuoteGenerator, 10000);
+
+		function randomQuoteGenerator () {
+
+		var randomQuoteRequest = new XMLHttpRequest();
+		randomQuoteRequest.open('GET', 'http://quotes.stormconsultancy.co.uk/random.json', true);
+		randomQuoteRequest.onload = function () {
+			var quotes = JSON.parse(randomQuoteRequest.responseText);
+
+
+			var quote = document.querySelector('.quote');
+			var author = document.querySelector('.author');
+
+			quote.innerHTML = quotes.quote;
+			author.innerHTML = '-' + quotes.author;
+
+			//jQuery for Fade in and Fade out of random quotes
+			setTimeout(function() {
+
+				$(".quote").ready(function() {
+					setTimeout(function() {
+						$(".quote").fadeOut(3050);
+						$(".author").fadeOut(3050);
+					})
+
+					setTimeout(function() {
+						$(".quote").fadeIn(3000);
+						$(".author").fadeIn(3000);
+					},3050)
+
+				})
+				
+			}, 7000);
+
+			
+		};
+
+		randomQuoteRequest.send();
+
+		};
+
+
+
+}
+
 
 
 
